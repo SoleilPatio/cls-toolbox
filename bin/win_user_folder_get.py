36e55291f7cf555dc90611600ddde745223afeb6
ELPIS <ELPIS@yuntin.com>
2020-03-04 11:35:16 +0800

    patch

--- bin/win_user_folder_get.py
+++ bin/win_user_folder_get.py
@@ -6,12 +6,12 @@ import sys
 import os
 
 def WinUserFolderGet( value_name ):
-    output = subprocess.check_output(['reg', 'query', 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders', '/v',  value_name])
-    match_list = re.findall("%s\s+\w+\s+([%%\w: \\\\]*)" % value_name, output)
+    output = subprocess.check_output(['reg', 'query', r'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders', '/v',  value_name])
+    match_list = re.findall(r"%s\s+\w+\s+([%%\w: \\\\]*)" % value_name, output)
     return WinEnvVarResolve(match_list[0])
 
 def WinEnvVarResolve( input_str ):
-    for item in { x[0] : os.environ.get(x[1],x[0]) for x in re.findall("(%([\S^%]*)%)", input_str)}.items():
+    for item in { x[0] : os.environ.get(x[1],x[0]) for x in re.findall(r"(%([\S^%]*)%)", input_str)}.items():
         input_str = input_str.replace( item[0], item[1])
     return input_str
 
@@ -26,7 +26,7 @@ if __name__ == "__main__":
         parser.print_usage()
 
     if (args.list):
-        output = subprocess.check_output(['reg', 'query', 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders' ])
+        output = subprocess.check_output(['reg', 'query', r'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders' ])
         print output
 
     if (args.value_name):
