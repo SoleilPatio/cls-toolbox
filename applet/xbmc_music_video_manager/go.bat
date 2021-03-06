cad6c42423580aa43d1491fb7de9e3bd6d8cc71e
ATHENE <ATHENE@yuntin.com>
2021-03-06 21:48:31 +0800

    add xmbcmusicvideo go.bat

---
+++ applet/xbmc_music_video_manager/go.bat
@@ -0,0 +1,4 @@
+
+REM [Note] Big-5 encode
+python XbmcMusicVideoManager.py -r "C:\APN\MNEMOSYNE\NDK-Data\DB_AV_多媒體資料\VIDEO_影片\[MUSIC VIDEOS]"
+python XbmcMusicVideoManager.py -r "C:\APN\MNEMOSYNE\NDK-Data\DB_AV_多媒體資料\VIDEO_影片\[MUSIC VIDEOS] - KIDS"
