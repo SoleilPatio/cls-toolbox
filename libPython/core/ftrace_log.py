f3c29a0a6588c7bf5e350c4a310b4c4b8c714a11
clouds.lee <clouds@ATHENE>
2023-04-06 21:41:42 +0800

    ftrace support B/E,S/F,C type

--- libPython/core/ftrace_log.py
+++ libPython/core/ftrace_log.py
@@ -16,6 +16,7 @@ class FtraceLog():
         self.m_prog_name = prog_name
         self.m_logger = logging.getLogger("ftrace")
         self.m_logger.setLevel(logging.DEBUG)
+        self.m_pid = os.getpid()
 
         pass
 
@@ -58,7 +59,7 @@ class FtraceLog():
         #restore format:
         # FTRACE_FMT = "FTRACELOG-0 [0] ...1 %(asctime)s.%(msecs)03d: tracing_mark_write: %(message)s"
         # FTRACE_FMT = "FTRACELOG-0 [0] ...1 %(asctime)s.%(msecs)03d: %(message)s"
-        FTRACE_FMT = "FTRACELOG-0 [0] ...1 %(created)f: %(message)s"
+        FTRACE_FMT = "FTRACELOG-%(thread)d [0] ...1 %(created)f: %(message)s"
         
         stream_handler.setFormatter( logging.Formatter(fmt=FTRACE_FMT, datefmt="%S") )
         file_handler.setFormatter( logging.Formatter(fmt=FTRACE_FMT, datefmt="%S") )
@@ -66,10 +67,19 @@ class FtraceLog():
         return OUT_DIR, exe_log_filename
     
     def atrace_B(self, functionname ):
-        self.m_logger.error(f"tracing_mark_write: B|777|{functionname}")
+        self.m_logger.error(f"tracing_mark_write: B|{self.m_pid}|{functionname}")
 
     def atrace_E(self):
         self.m_logger.error("tracing_mark_write: E")
+
+    def atrace_S(self, functionname, cookie ):
+        self.m_logger.error(f"tracing_mark_write: S|{self.m_pid}|{functionname}|{cookie}")
+
+    def atrace_F(self, functionname, cookie ):
+        self.m_logger.error(f"tracing_mark_write: F|{self.m_pid}|{functionname}|{cookie}")
+
+    def atrace_C(self, name, count ):
+        self.m_logger.error(f"tracing_mark_write: C|{self.m_pid}|{name}|{count}")
 
 
 """
@@ -84,6 +94,7 @@ if __name__ == "__main__":
     
     from  libPython.core.ftrace_log import ftraceLog
     import time
+    import random
 
     ftraceLog.LogInitial(__file__)
     ftraceLog.atrace_B("SUMALL")
@@ -92,15 +103,20 @@ if __name__ == "__main__":
         ftraceLog.atrace_B("sum_one")
         
         time.sleep(1)
+        ftraceLog.atrace_C("random1", int(random.random()*1000))
 
         ftraceLog.atrace_B("X")
         time.sleep(1)
         ftraceLog.atrace_E()
 
         time.sleep(1)
+        ftraceLog.atrace_S("mul", i)
 
         ftraceLog.atrace_E()
         time.sleep(2)
+
+        ftraceLog.atrace_F("mul", i)
+        ftraceLog.atrace_C("random2", int(random.random()*1000))
 
     ftraceLog.atrace_E()
 
